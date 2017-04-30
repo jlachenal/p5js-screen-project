9d92653bdca04993a28fd94d1c2e23cc413391df
sylviakong <9f:c6:e9:c2:81:b4:72:ac:19:5f:11:71:04:32:38:01>
2017-04-30 13:33:51 -0700

    update

--- sylvia/sketch.js
+++ sylvia/sketch.js
@@ -1,35 +1,66 @@
+var starsgroups = [];
 var stars = [];
-var paingting = false;
-var secsdelay;
+var previous;
+var img;
+
+// var paingting = false;
+// var secsdelay;
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(361, 641);
+  starsgroups.push(stars);
+  previous = millis();
+  img = loadImage('image/bg.jpg');
 }
 
 function mouseClicked() {
+  var now = millis();
+  if(now - previous > 700){
+    stars = [];
+    starsgroups.push(stars);
+  }
+  previous = now;
+
   var star = {};
   star.x = mouseX;
   star.y = mouseY;
+  star.alpha = 255;
   stars.push(star);
   console.log(stars);
 
 }
 
+
 function draw() {
-  clear();
-  fill(255);
-  for(var i = 0; i < stars.length; i++){
-    if (i != stars.length - 1){
-    line(stars[i].x, stars[i].y, stars[i+1].x, stars[i+1].y);
+  // background(0);
+  // PImage img;
+  // img = loadImage('bg.jpg');
+  // background(img);
+  // background(img);
+  image(img, 0, 0);
+  // image =  ;
+  // clean();
+  var star;
+  for (var g = 0; g < starsgroups.length; g++) {
+    var stars = starsgroups[g];
+    for(var i = 0; i < stars.length; i++) {
+      star = stars[i];
+      if (i != stars.length - 1){
+        stroke(255,255,255,star.alpha);
+        line(star.x, star.y, stars[i+1].x, stars[i+1].y);
+      }
+      fill(255,255,255,star.alpha);
+      ellipse(star.x, star.y, 5, 5);
+      star.y = star.y - 0.5;
+      star.alpha = star.alpha - 0.8;
+    }
   }
-    ellipse(stars[i].x, stars[i].y, 5, 5);
-    stars[i].y = stars[i].y - 1;
-    opacity
-  }
+}
+
 //  var secdelay {
 //    if mousePress
 //  }
-}
+
 
 
 //function secsdelay() {
